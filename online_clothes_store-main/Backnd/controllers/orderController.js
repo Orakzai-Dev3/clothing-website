9f90ec7bedb426e8cbbe9a5a77f6108836123089
Orakzai-Dev3 <arehman.dev3@gmail.com>
2026-01-20 14:05:47 +0500

    added online clothes store project files properly

---
+++ online_clothes_store-main/Backnd/controllers/orderController.js
@@ -0,0 +1,98 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+import Stripe from 'stripe'
+import razorpay from 'razorpay'
+
+// global variables
+const currency = 'inr'
+const deliveryCharge = 50
+
+// gateway initialize
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+const razorpayInstance = new razorpay({
+    key_id : process.env.RAZORPAY_KEY_ID,
+    key_secret : process.env.RAZORPAY_KEY_SECRET,
+})
+
+// Placing orders using COD Method
+const placeOrder = async (req,res) => {
+    
+    try {
+        
+        const { userId, items, amount, address} = req.body;
+
+        const orderData = {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod:"COD",
+            payment:false,
+            date: Date.now()
+        }
+
+        const newOrder = new orderModel(orderData)
+        await newOrder.save()
+
+        await userModel.findByIdAndUpdate(userId,{cartData:{}})
+
+        res.json({success:true,message:"Order Placed"})
+
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+
+}
+
+
+
+
+// All Orders data for Admin Panel
+const allOrders = async (req,res) => {
+
+    try {
+        
+        const orders = await orderModel.find({})
+        res.json({success:true,orders})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+
+}
+
+// User Order Data For Forntend
+const userOrders = async (req,res) => {
+    try {
+        
+        const { userId } = req.body
+
+        const orders = await orderModel.find({ userId })
+        res.json({success:true,orders})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
+// update order status from Admin Panel
+const updateStatus = async (req,res) => {
+    try {
+        
+        const { orderId, status } = req.body
+
+        await orderModel.findByIdAndUpdate(orderId, { status })
+        res.json({success:true,message:'Status Updated'})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
+export { placeOrder, allOrders, userOrders, updateStatus}
